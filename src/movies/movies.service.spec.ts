8676266ce189e2d7d3c4deaee697ff8e83f16d72
Ha young, Kim <jeremiah91@naver.com>
2022-03-15 16:27:41 +0900

    test : Unit test  - perform unit test on functions( getAll(), getOne() )  - create mock data for testing  - handling an error case
    *토스에서 발표했던 test-coverage 100% 란 정말 말도안되게 놀라운 일이었음을 느낌

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -14,5 +15,37 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // movie 목록을 가져올 때, 해당 목록의 타입이 배열인지 테스트
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  // movie 하나를 가져올 때,
+  describe('getOne', () => {
+    it('sholud return a movie', () => {
+      // test 를 위한 mock data 생성
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 1991,
+      });
+
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+    });
+
+    // error case
+    it('should throw a 404 error', () => {
+      try {
+        service.getOne(9999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
