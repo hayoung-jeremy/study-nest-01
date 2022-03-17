3e278aebf02e78b4517bad96a4f0893770ebb7d0
Ha young, Kim <jeremiah91@naver.com>
2022-03-17 16:53:14 +0900

    test : End to End Test  - GET, POST, DELETE

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -14,11 +14,30 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
-
+  // sending API request
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to the Movie API');
+  });
+
+  describe('/moives', () => {
+    // 테스트에 필요한 database 와 평소 사용하는 database 따로 있음
+    // 테스팅 database 에서는 데이터를 생성, 삭제하는 일이 빈번함
+    // 처음부터 빈 database 이기 때문에 아래와 같이 테스트함
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ title: 'TEST', year: 1991, genres: ['TEST'] })
+        .expect(201);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
 });
