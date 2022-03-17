630ad7c6f4b2eaa7ba550e28928a08e9a2184ae9
Ha young, Kim <jeremiah91@naver.com>
2022-03-17 14:49:49 +0900

    test : Unit Test -02  - create, delete, update

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -48,4 +48,69 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  // movie 삭제
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      // test 를 위한 mock data 생성
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 1991,
+      });
+      const beforeMovies = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+
+      expect(afterDelete).toBeLessThan(beforeMovies);
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.deleteOne(9999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  // movie 생성
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      // test 를 위한 mock data 생성
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 1991,
+      });
+      const afterCreate = service.getAll().length;
+
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  // movie 업데이트
+  describe('update', () => {
+    it('should update a movie', () => {
+      // test 를 위한 mock data 생성
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 1991,
+      });
+
+      service.update(1, { title: 'Updated Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.update(9999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
